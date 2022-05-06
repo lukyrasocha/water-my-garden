a3c0d489929daf10964cef75c0722dbbe892312e
Lukas Rasocha <rasocha.lukas@gmail.com>
2022-05-06 08:56:20 +0200

    Find optimal values for the pump

--- project/project.ino
+++ project/project.ino
@@ -1,12 +1,23 @@
-void setup() {
-  // initialize serial communication at 9600 bits per second:
+int motorPin = 3; // pin that turns on the motor
+int blinkPin = 13; // pin that turns on the LED
+int watertime = 1; // how long it will be watering (in seconds)
+int waittime = 1; // how long to wait between watering (in minutes)
+bool flag = true;
+
+void setup()
+{
+  pinMode(motorPin, OUTPUT); // set Pin 3 to an output
+  pinMode(blinkPin, OUTPUT); // set pin 13 to an output
   Serial.begin(9600);
+  //digitalWrite(motorPin, HIGH);
+  delay(watertime * 1000);  
+  //digitalWrite(motorPin, LOW); 
 }
 
-void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println(sensorValue);
-  delay(1000);        // delay in between reads for stability
+void loop()
+{
+  int moisturePin = analogRead(A0); //read analog value of moisture sensor
+  int moisture = ( 100 - ( (moisturePin / 1023.00) * 100 ) ); //convert analog value to percentage
+  Serial.println(moisture);
+  
 }
